1e7d6a1d7319eaa5aea3bfb870a68be9e74ca6f1
Jorge Velez <jorgeluisvelez@gmail.com>
2023-01-21 05:48:06 -0800

    primera conexion

---
+++ Arduino/OSCEthernetFastLEDReceiver/OSCEthernetFastLEDReceiver.ino
@@ -0,0 +1,186 @@
+
+#include <Arduino.h>
+#include <FastLED.h>
+#include <ETH.h>
+
+#include <OSCMessage.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+static bool eth_connected = false;
+static bool artnet_connected = false;
+
+// UDP specific
+ WiFiUDP Udp;
+#define ART_NET_PORT 6454
+int update_rate = 16;
+
+// LED settings
+const int clockPin = 4;//D5 esp32
+#define NUM_STRIPS 8
+#define NUM_LEDS_PER_STRIP 216
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+
+CRGB leds[NUM_LEDS];
+
+uint8_t RedB;
+uint8_t GreenB;
+uint8_t BlueB;
+
+int brightness = 200;
+
+unsigned long DELAY_TIME = 0; // 10 sec
+unsigned long INTERVAL = 1000;
+
+int packetCounter = 0;
+int frameCounter = 0;
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      initialize();
+
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+
+void initTest()
+{
+  Serial.println("");
+  Serial.println("Testing red1");
+  for (int i = 0 ; i < NUM_LEDS ; i++) {
+    leds[i] = CRGB(127, 0, 0);
+  }
+  FastLED.show();
+  delay(500);
+  Serial.println("Testing green");
+
+  for (int i = 0 ; i < NUM_LEDS ; i++) {
+    leds[i] = CRGB(0, 127, 0);
+  }
+  FastLED.show();
+  delay(500);
+  Serial.println("Testing blue");
+
+  for (int i = 0 ; i < NUM_LEDS ; i++) {
+    leds[i] = CRGB(0, 0, 127);
+  }
+  FastLED.show();
+  delay(500);
+  Serial.println("start listen ing");
+
+  for (int i = 0 ; i < NUM_LEDS ; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.onEvent(WiFiEvent);
+  ETH.begin();
+  //ETH.config(IPAddress(192, 168, 1, 9),IPAddress(192, 168, 1, 1),IPAddress(255, 255, 255, 0),IPAddress(192, 168, 1, 1),IPAddress(192, 168, 1, 1));
+
+}
+
+void initialize()
+{
+  Serial.print("initializing:");
+
+  Udp.begin(ART_NET_PORT);
+
+  FastLED.addLeds<APA102, 2, clockPin , BGR>(leds, 0, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 14, clockPin, BGR>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 15, clockPin, BGR>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 12, clockPin, BGR>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 13, clockPin, BGR>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 32, clockPin, BGR>(leds, 5 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<APA102, 33, clockPin, BGR>(leds, 6 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  //FastLED.addLeds<APA102, 39, clockPin, BGR>(leds, 7 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
+
+  FastLED.setBrightness(brightness);
+
+  initTest();
+}
+
+void ledtoggle(OSCMessage &msg) {
+  switch (msg.getInt(0)) {
+  case 0:
+    Serial.print("llego 0");
+    break;
+  case 1:
+    Serial.print("llego 1");
+    break;
+  }
+}
+
+void receiveMessage() {
+  OSCMessage inmsg;
+  int size = Udp.parsePacket();
+
+  if (size > 0) {
+    while (size--) {
+      inmsg.fill(Udp.read());
+    }
+    if (!inmsg.hasError()) {
+      inmsg.dispatch("/led", ledtoggle);
+      packetCounter++;
+    } 
+    else { auto error = inmsg.getError(); }
+  }
+}
+
+void loop() {
+  receiveMessage();
+  delay(update_rate);
+
+  if (millis()  >= (DELAY_TIME + INTERVAL) && eth_connected) {
+    DELAY_TIME = millis(); // finished delay -- single shot, once only
+    Serial.print("frames: ");
+    Serial.print(frameCounter);
+    Serial.print("packets: ");
+    Serial.println(packetCounter);
+    frameCounter = 0;
+    packetCounter = 0;
+
+    Serial.print("ETH MAC: ");
+    Serial.print(ETH.macAddress());
+    Serial.print(", IPv4: ");
+    Serial.print(ETH.localIP());
+  }
+
+}
---
+++ Max/controladorBarras.maxpat
@@ -0,0 +1,122 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 8,
+			"minor" : 2,
+			"revision" : 1,
+			"architecture" : "x64",
+			"modernui" : 1
+		}
+,
+		"classnamespace" : "box",
+		"rect" : [ 94.0, 85.0, 666.0, 905.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 1,
+		"objectsnaponopen" : 1,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 0,
+		"toptoolbarpinned" : 0,
+		"righttoolbarpinned" : 0,
+		"bottomtoolbarpinned" : 0,
+		"toolbars_unpinned_last_save" : 0,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "",
+		"subpatcher_template" : "",
+		"assistshowspatchername" : 0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 13.0,
+					"id" : "obj-9",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 19.75, 130.0, 53.0, 23.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-15",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 19.75, 204.0, 85.0, 22.0 ],
+					"text" : "prepend /led 1"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 13.0,
+					"format" : 6,
+					"id" : "obj-8",
+					"maxclass" : "flonum",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 93.75, 134.0, 54.0, 23.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 13.0,
+					"id" : "obj-24",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 20.0, 263.0, 163.0, 23.0 ],
+					"text" : "udpsend 192.168.1.2 6454"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-24", 0 ],
+					"source" : [ "obj-15", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-15", 0 ],
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-15", 0 ],
+					"source" : [ "obj-9", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ],
+		"autosave" : 0
+	}
+
+}
